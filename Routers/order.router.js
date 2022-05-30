8b72573a34dddb04f48910b8d5d4d380d8d758ab
Rafael Basterra Careaga <rbasterra@msn.com>
2022-05-30 23:54:37 +0200

    changed OrderRouter post method. Changed also Order schema

--- Routers/order.router.js
+++ Routers/order.router.js
@@ -1,45 +1,63 @@
 const express = require("express"); // required express for using the npm hta.
 const router = express.Router();// required jsonwebtoken for using the function of espress hta.
-const orderSchema = require("../Models/order.model");// required for using model hta.
+const Order = require("../Models/order.model");// required for using model hta.
 const { check, validationResult } = require('express-validator');// required express-validator for using the npm hta.
 
 // to create product
-router.post("/",
-    [
-        check('nº', 'nº is required')
-            .not()
-            .isEmpty(),
-        check('product')
-            .not()
-            .isEmpty(),
-            check('product')
-            .not()
-            .isEmpty('user')
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
+// router.post("/",
+//     [
+//         check('nº', 'nº is required')
+//             .not()
+//             .isEmpty(),
+//         check('product')
+//             .not()
+//             .isEmpty(),
+//             check('product')
+//             .not()
+//             .isEmpty('user')
+//     ],
+//     (req, res, next) => {
+//         const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            return res.status(422).json(errors.array());
-        }
-        else {
-            const product = new orderSchema({
-                nº: req.body.nº,
-                product: req.body.product,
-                user: rec.body.user,
-            });
-            product.save().then((response) => {
-                res.status(201).json({
-                    message: "product successfully created!",
-                    result: response
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    error: error
-                });
-            })
-        };
+//         if (!errors.isEmpty()) {
+//             return res.status(422).json(errors.array());
+//         }
+//         else {
+//             const product = new orderSchema({
+//                 nº: req.body.nº,
+//                 product: req.body.product,
+//                 user: rec.body.user,
+//             });
+//             product.save().then((response) => {
+//                 res.status(201).json({
+//                     message: "product successfully created!",
+//                     result: response
+//                 });
+//             }).catch(error => {
+//                 res.status(500).json({
+//                     error: error
+//                 });
+//             })
+//         };
+// });
+
+router.post('/', (req, res, next) => {
+
+    const newOrder = new Order({
+        products: [...req.body.products],
+        userId: req.body.user
     });
+
+    return newOrder.save()
+            .then(() => res.status(201).json(newOrder))
+            .catch((err) => {
+                const error = new Error(err);
+                error.status = 500;
+                return next(error);
+            });
+
+});
+
 router.route('/').get((req, res) => {
     orderSchema.find((error, response) => {
         if (error) {
